b7e62aa137a1867fdff21eb6041e274d529cf87d
Joel Azemar <joel.azemar@gmail.com>
2023-11-23 15:37:16 +0100

    Update template.rb

--- config/template.rb
+++ config/template.rb
@@ -1,31 +1,12 @@
 apply "config/application.rb"
 template "config/database.yml", force: true
 remove_file "config/secrets.yml"
-copy_file "config/sidekiq.yml"
-
-insert_into_file "config/puma.rb", <<~RUBY, after: /:tmp_restart$/
-
-
-  # Automatically open the browser when in development
-  require_relative "../lib/puma/plugin/open"
-  plugin :open
-RUBY
-
-gsub_file "config/routes.rb", /  # root 'welcome#index'/ do
-  '  root "home#index"'
-end
 
 gsub_file "config/routes.rb", /  # root 'welcome#index'/ do
   '  root "home#index"'
 end
 
 copy_file "config/initializers/generators.rb"
-copy_file "config/initializers/version.rb"
-copy_file "config/initializers/sidekiq.rb"
-
-gsub_file "config/initializers/filter_parameter_logging.rb", /\[:password\]/ do
-  "%w[password secret session cookie csrf]"
-end
 
 apply "config/environments/development.rb"
 apply "config/environments/production.rb"
