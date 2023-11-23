bc7e15346269070e61c01a17d38d4996da98d1ad
Joel Azemar <joel.azemar@gmail.com>
2023-11-23 15:36:58 +0100

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,8 +5,9 @@ insert_into_file "config/environments/development.rb", after: mailer_regex do
   <<-RUBY
 
   # Ensure mailer works in development.
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {address: "127.0.0.1", port: 1025, domain: "localhost"}
   config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.asset_host = "http://localhost:3000"
   RUBY
