d7d507822b0352f052666fc6c1b05b5c24210e57
Joel Azemar <joel.azemar@gmail.com>
2023-11-24 23:04:24 +0100

    update

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -2,4 +2,5 @@ Rails.application.config.generators do |generator|
   # Disable generators we don't need.
   generator.javascripts false
   generator.stylesheets false
+  generator.orm :active_record, primary_key_type: :uuid
 end
