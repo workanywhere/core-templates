5cc9c128205b0617f1edb135d2467ff6caf8b836
Joel Azemar <joel.azemar@gmail.com>
2023-11-24 18:42:17 +0100

    Update template.rb

--- config/template.rb
+++ config/template.rb
@@ -2,15 +2,8 @@ apply "config/application.rb"
 
 remove_file "config/secrets.yml"
 
-gsub_file "config/routes.rb", /  # root 'welcome#index'/ do
-  '  root "home#index"'
-end
-
 copy_file "config/initializers/generators.rb"
 
 apply "config/environments/development.rb"
 apply "config/environments/production.rb"
 apply "config/environments/test.rb"
-
-route 'root "home#index"'
-route %Q(mount Sidekiq::Web => "/sidekiq" if defined?(Sidekiq) # monitoring console\n)
