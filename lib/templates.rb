6db2218661a4122763a5856a0044393bc5e9eb2e
Joel Azemar <joel.azemar@gmail.com>
2023-11-25 12:44:46 +0100

    Update templates.rb

--- lib/templates.rb
+++ lib/templates.rb
@@ -1,11 +1,23 @@
-Dir[File.join(File.dirname(__FILE__), "templates", "**", "*")].each do |file|
-  if File.directory?(file)
-    empty_directory file.split("lib/templates/").last
+require "fileutils"
+require "pathname"
 
-    next
+# Main method to process the templates
+def process_templates
+  @templates_dir = Pathname.new(__dir__).join("templates")
+  destination_root = Pathname.new("lib/templates")
+
+  @templates_dir.glob("**/*").each do |file|
+    process_file(file, destination_root) if file.file?
   end
-
-  file_destination = File.join("lib/templates", File.dirname(file).split("lib/templates/").last, File.basename(file))
-  copy_file file, file_destination
-  git_commit "Add template #{file_destination}"
 end
+
+# Method to copy a file to the destination and commit the change
+def process_file(file, destination_root)
+  file_destination = destination_root.join(file.relative_path_from(@templates_dir))
+  FileUtils.mkdir_p(file_destination.dirname)
+  FileUtils.cp(file, file_destination)
+  git_commit("Add template #{file_destination}")
+end
+
+# Run the main method
+process_templates
