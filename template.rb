ceefba83bbcfb993dc51a5a6863b6121027f9ded
Joel AZEMAR <joel.azemar@gmail.com>
2023-12-28 11:47:51 +0100

    Update template.rb

--- template.rb
+++ template.rb
@@ -59,6 +59,12 @@ def apply_template!
   apply "lib/templates.rb"
   git_commit "Add templates"
 
+  # Make sure the templates are NOT loaded.
+  # Ideally template files should get the extension .tt, so there are ignored by default.
+  # Unfortunately it is not always the case so we have to make sure the directory is safely ignored by the loader.
+  gsub_file "config/application.rb", /config\.autoload_lib\(ignore: %w\(assets tasks\)\)/, "config.autoload_lib(ignore: %w[assets tasks templates])"
+  git_commit "Ignore templates"
+
   empty_directory_with_keep_file "app/lib"
 
   empty_directory ".git/safe"
@@ -277,7 +283,10 @@ def rewrite_json(file)
 end
 
 def git_commit(message)
-  return unless changes_to_commit?
+  if !changes_to_commit?
+    say "No changes to commit for #{message}", :yellow
+    return
+  end
 
   git add: "-A ."
   git commit: "-n -m '#{message}'"
