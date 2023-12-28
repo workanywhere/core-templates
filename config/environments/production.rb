78ba029716de6b07782ca79667be739af6653562
Joel AZEMAR <joel.azemar@gmail.com>
2023-12-28 11:47:30 +0100

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,9 @@ uncomment_lines "config/environments/production.rb", /config\.force_ssl = true/
 uncomment_lines "config/environments/production.rb", /config\.active_job/
 uncomment_lines "config/environments/production.rb", /raise_delivery_errors =/
 
+comment_lines "config/environments/production.rb", /config\.active_job\.queue_adapter =/
+comment_lines "config/environments/production.rb", /config\.active_job\.queue_name_prefix =/
+
 gsub_file "config/environments/production.rb", /raise_delivery_errors = false/, "raise_delivery_errors = true"
 gsub_file "config/environments/production.rb", /\bSTDOUT\b/, "$stdout"
 gsub_file "config/environments/production.rb",
