89275881af9902d40874f7d41bfbcdb633adb55d
Joel AZEMAR <joel.azemar@gmail.com>
2025-01-10 14:51:33 +0100

    CI Headless System Test

--- spec/support/system_test.rb
+++ spec/support/system_test.rb
@@ -2,7 +2,8 @@
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :rack_test # rack_test by default, for performance
+    # driven_by :rack_test # rack_test by default, for performance
+    driven_by (ENV["TEST_BROWSER"] || :selenium_chrome).to_sym, screen_size: [1400, 1400]
   end
 
   config.before(:each, :js, type: :system) do
